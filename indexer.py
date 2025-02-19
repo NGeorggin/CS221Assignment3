55c23ab2a999c5d13b9107ad3ddb285658bd46af
NGeorggin <ngeorggi@uci.edu>
2025-02-19 14:59:57 -0800

    Inverted Index Construction

--- indexer.py
+++ indexer.py
@@ -3,6 +3,7 @@ import nltk
 import bs4
 import json
 import os # TODO cite os walk
+import re
 
 invertedIndex = dict()
 
@@ -16,8 +17,18 @@ invertedIndex = dict()
 # content = json.load(open(".\\DEV\\alderis_ics_uci_edu\\0f274aaa945c05641a9677b951c32026bb201ec9aeb6e691fedd1235b3a5d6af.json"))
 # print(content)
 
+# key=t, val=[Posting(docid, tf)]
+class Posting:
+    def __init__(self, docid, tf):
+        self.docid = docid
+        self.tf = tf
+    def __repr__(self):
+        return f"Posting(docid={self.docid},tf={self.tf})"
+
+invertedIndex = dict()
+
 fullWalk = [i for i in os.walk(".\\DEV") if len(i[1]) == 0]
-for subdir in fullWalk:
+for i, subdir in enumerate(fullWalk):
     print(subdir)
     for file in subdir[2]:
         fileName = f".\\{subdir[0]}\\{file}"
@@ -29,6 +40,31 @@ for subdir in fullWalk:
             allTitles = bsObject.find_all('title') # TODO Check
             
             allText = bsObject.get_text()
+            ## TOKENIZE THE TEXT FROM Assignment 2
+            wordFreq = {x.lower():0 for x in nltk.word_tokenize(allText) if re.match(r"^[a-zA-Z0-9]+$", x)}
+            for word in nltk.word_tokenize(allText):
+                if re.match(r"^[a-zA-Z0-9]+$", word):
+                    wordFreq[word.lower()] += 1
+            # Go through each token and record frequency from part 1 code and get tf from that
+
+            for token in wordFreq.keys():
+                ## CALCULATE TF
+                if token not in invertedIndex:
+                    invertedIndex[token] = [Posting(i, wordFreq[token])]
+                else:
+                    invertedIndex[token].append(Posting(i, wordFreq[token]))
+
+            # print(invertedIndex)
+        if len(invertedIndex) > 10000:
+            break
+    if len(invertedIndex) > 10000:
+        break
+    #     break
+    # break
+
+
+print(invertedIndex)
+
             
             
 
