5a718c8758314b9ca4ebf3d0826e52c2788b9439
NGeorggin <ngeorggi@uci.edu>
2025-02-21 20:26:09 -0700

    Added a proper counter for the mapping

--- indexer.py
+++ indexer.py
@@ -59,12 +59,14 @@ def json_combine(key_list: list, new_index: dict, disk_index: dict) -> dict:
 fullWalk = [i for i in os.walk(".\\DEV") if len(i[1]) == 0]
 
 docMapping = dict()
+j = 0
 for i, subdir in enumerate(fullWalk):
     # print(subdir)
     for file in subdir[2]:
         # print(file)
         fileStringName = re.match(r'[a-zA-Z0-9]+', file).group()
-        docMapping[fileStringName] = i
+        docMapping[fileStringName] = j
+        j += 1
 
 for i, subdir in enumerate(fullWalk):
     print(subdir)
