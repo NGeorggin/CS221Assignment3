34963aed17f245a8549d0e4d210df35bda1bca84
NGeorggin <ngeorggi@uci.edu>
2025-02-21 19:14:47 -0800

    Removed some debug-by-print statements

--- indexer.py
+++ indexer.py
@@ -60,9 +60,9 @@ fullWalk = [i for i in os.walk(".\\DEV") if len(i[1]) == 0]
 
 docMapping = dict()
 for i, subdir in enumerate(fullWalk):
-    print(subdir)
+    # print(subdir)
     for file in subdir[2]:
-        print(file)
+        # print(file)
         fileStringName = re.match(r'[a-zA-Z0-9]+', file).group()
         docMapping[fileStringName] = i
 
